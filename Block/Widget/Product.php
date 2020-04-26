980c19f1ea92b76a669538e853eeb1ad75084e82
Alex Dong <support@alothemes.com>
2020-04-26 17:08:03 +0700

    Support Quickedit

--- Block/Widget/Product.php
+++ Block/Widget/Product.php
@@ -1,12 +1,12 @@
 <?php
 /**
  * Magiccart 
- * @category 	Magiccart 
- * @copyright 	Copyright (c) 2014 Magiccart (http://www.magiccart.net/) 
- * @license 	http://www.magiccart.net/license-agreement.html
+ * @category    Magiccart 
+ * @copyright   Copyright (c) 2014 Magiccart (http://www.magiccart.net/) 
+ * @license     http://www.magiccart.net/license-agreement.html
  * @Author: DOng NGuyen<nguyen@dvn.com>
  * @@Create Date: 2016-01-05 10:40:51
- * @@Modify Date: 2018-06-27 17:20:42
+ * @@Modify Date: 2020-04-26 17:20:42
  * @@Function:
  */
 
@@ -14,60 +14,114 @@ namespace Magiccart\Lookbook\Block\Widget;
 
 class Product extends \Magento\Catalog\Block\Product\AbstractProduct implements \Magento\Widget\Block\BlockInterface
 {
-
     /**
      * @var \Magento\Framework\Url\Helper\Data
      */
     protected $urlHelper;
 
-    protected $_storeManager;
+     /**
+     * @var \Magento\Backend\Model\UrlInterface
+     */
+    protected $backendUrl;
 
     protected $_productCollectionFactory;
 
-	protected $_lookbook;
+    protected $_lookbook;
 
     protected $_typeId = '1';
 
     /**
+     * @var \Magiccart\Lookbook\Model\LookbookFactory
+     */
+    protected $lookbookFactory;
+
+    /**
      * @param Context $context
-     * @param \Magento\Framework\ObjectManagerInterface $objectManager
+     * @param \Magento\Framework\Url\Helper\Data $urlHelper
+     * @param \Magento\Backend\Model\UrlInterface $backendUrl
      * @param \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory
      * @param \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility
+     * @param \Magiccart\Lookbook\Model\LookbookFactory
      * @param array $data
      */
     public function __construct(
         \Magento\Catalog\Block\Product\Context $context,
         \Magento\Framework\Url\Helper\Data $urlHelper,
+        \Magento\Backend\Model\UrlInterface $backendUrl,
         \Magento\Catalog\Model\ResourceModel\Product\CollectionFactory $productCollectionFactory,
         \Magento\Catalog\Model\Product\Visibility $catalogProductVisibility,
-        \Magiccart\Lookbook\Model\Lookbook $lookbook,
+        \Magiccart\Lookbook\Model\LookbookFactory $lookbookFactory,
         array $data = []
     ) {
-        $this->urlHelper = $urlHelper;
+        $this->urlHelper    = $urlHelper;
+        $this->backendUrl   = $backendUrl;
         $this->_productCollectionFactory = $productCollectionFactory;
         $this->_catalogProductVisibility = $catalogProductVisibility;
-        $this->_lookbook = $lookbook;
+        $this->lookbookFactory = $lookbookFactory;
         parent::__construct( $context, $data );
     }
 
     protected function _construct()
     {
-		$store = $this->_storeManager->getStore()->getStoreId();
+        $store = $this->_storeManager->getStore()->getStoreId();
         $identifier = $this->getIdentifier();
-        $collection = $this->_lookbook->getCollection()->addFieldToSelect('*')
+        $collection = $this->lookbookFactory->create()->getCollection()->addFieldToSelect('*')
                         ->addFieldToFilter('identifier', $identifier)
                         ->addFieldToFilter('type_id', $this->_typeId)
                         ->addFieldToFilter('stores',array( array('finset' => 0), array('finset' => $store)));
         $collection->getSelect()->order('order','ASC');
 
-        $item = $collection->getFirstItem();
-        if(!$item){
+        $this->_lookbook = $collection->getFirstItem();
+        if(!$this->_lookbook){
             echo '<div class="message-error error message">Identifier "'. $identifier . '" not exist.</div> ';          
             return;
         }
-        $data = $item->getData();
+        $data = $this->_lookbook->getData();
         $this->addData($data);
         parent::_construct();
+    }
+
+    public function getLookbook()
+    {
+        return $this->_lookbook;
+    }
+
+    public function getAdminUrl($adminPath, $routeParams=[], $storeCode = 'default' ) 
+    {
+        $routeParams[] = [ '_nosid' => true, '_query' => ['___store' => $storeCode]];
+        return $this->backendUrl->getUrl($adminPath, $routeParams);
+    }
+
+    public function getQuickedit()
+    {
+        $lookbook = $this->getLookbook();
+        if($lookbook){
+            $id = $lookbook->getId();
+            $routeParams = [
+                'lookbook_id' => $id
+            ];
+            $class      = basename(__FILE__, ".php");
+            $adminPath  = 'lookbook/' . strtolower($class) . '/edit';
+            $editUrl    = $this->getAdminUrl($adminPath, $routeParams);
+            $moduleName = $this->getModuleName();
+            $moduleName = str_replace('_', ' > ', $moduleName);
+            $quickedit  = [
+                [
+                    'title' => __('%1 > %2 Id is: %3', $moduleName, $class, $id),
+                    'url'   => $editUrl
+                ],
+                [
+                    'title' => __('%1 > %2 Identifier is: %3', $moduleName, $class, $lookbook->getIdentifier()),
+                    'url'   => $editUrl
+                ],
+                [
+                    'title' => __('Edit'),
+                    'url'   => $editUrl
+                ]
+            ];
+        }
+
+        return $quickedit;      
     }
 
     public function getProductCollection($producIds)
